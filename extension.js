456219df7e7ba0394d8ba6ffbc193cc3f5e4d230
Kamil Marszałkowski <marshal@wp-sa.pl>
2013-01-11 11:25:15 +0100

    Panel button added

--- extension.js
+++ extension.js
@@ -2,53 +2,72 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
-
-let text, button;
-
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
-
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
-    }
-
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(Math.floor(monitor.width / 2 - text.width / 2),
-                      Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
-}
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
 
 function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run',
-                             icon_type: St.IconType.SYMBOLIC,
-                             style_class: 'system-status-icon' });
-
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
+	return new ExtensionController();
 }
 
-function enable() {
+/*function enable() {
     Main.panel._rightBox.insert_child_at_index(button, 0);
 }
 
 function disable() {
     Main.panel._rightBox.remove_child(button);
+}*/
+
+// =========
+function ExtensionController() {
+	return {
+		//extensionMeta: extensionMeta,
+		extension: null,
+		settings: null,
+
+		enable: function() {
+			this.extension = new RadeonExtension();
+
+			Main.panel._rightBox.insert_child_at_index(this.extension.actor, 0);
+			//Main.panel._menus.addMenu(this.extension.menu);
+		},
+
+		disable: function() {
+			Main.panel._rightBox.remove_actor(this.extension.actor);
+			//Main.panel._menus.removeMenu(this.extension.menu);
+			
+			// Causing reference error
+			//this.extension.actor.destory(); 
+		}
+	}
 }
+
+function RadeonExtension() {
+	this._init();
+}
+
+RadeonExtension.prototype = {
+	__proto__: PanelMenu.Button.prototype,
+
+	_init: function() {
+		PanelMenu.Button.prototype._init.call(this, 0.0);
+
+		//this.extensionMeta = extensionMeta;
+		
+		this.panelContainer = new St.BoxLayout();
+		this.actor.add_actor(this.panelContainer);
+
+		this.panelLabel = new St.Label({style_class: "radeon-label", text: _("Radeon")});
+		this.currentActivity = null;
+
+		// icon
+		this.icon = new St.Icon({ icon_name: 'system-run',
+                             icon_type: St.IconType.SYMBOLIC,
+                             style_class: 'system-status-icon' });
+
+		this.panelContainer.add(this.icon);
+		this.panelContainer.add(this.panelLabel);
+
+	},
+
+}
+
