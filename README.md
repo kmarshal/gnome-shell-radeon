71e93541e0e639b08a98b111db004b4d3ab2581f
Kamil Marszałkowski <k.marszalkowski@gmail.com>
2013-01-15 22:28:11 +0100

    Slider changing current fan speed.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 gnome-shell-radeon
 ==================
 
-Gnome-shell extension
+Gnome-shell extension to control fan speed on Radeon GPU with proprietary drivers.
+Display current GPU temperature.
+Require: aticonfig.
--- extension.js
+++ extension.js
@@ -1,3 +1,4 @@
+// author: Kamil Marszałkowski
 
 const St = imports.gi.St;
 const Main = imports.ui.main;
@@ -8,148 +9,112 @@ const GLib = imports.gi.GLib;
 const Lang = imports.lang;
 const Mainloop = imports.mainloop
 
-var ext;
-var btn;
-
 function init() {
 	return new ExtensionController();
-	//ext = new RadeonExtension();
 }
 
-/*function enable() {
-
-	btn = new PanelMenu.Button({label: "label"});
-	btn.label = "dasd";
-	let item = new PopupMenu.PopupSliderMenuItem(0);
-	btn.menu.addMenuItem(item);
-    Main.panel._rightBox.insert_child_at_index(btn.actor, 0);
-}
-
-function disable() {
-    //Main.panel._rightBox.remove_child(ext.actor);
-    Main.panel._rightBox.remove_child(btn.actor);
-	//GLib.source_remove(ext.event);
-}*/
-
-// =========
 function ExtensionController() {
 	return {
-		//extensionMeta: extensionMeta,
 		extension: null,
 		settings: null,
 
 		enable: function() {
 			this.extension = new RadeonExtension();
-
 			Main.panel._rightBox.insert_child_at_index(this.extension.actor, 0);
 			Main.panel._menus.addMenu(this.extension.menu);
 		},
 
 		disable: function() {
 			Main.panel._rightBox.remove_actor(this.extension.actor);
-			GLib.source_remove(this.extension.event);
 			Main.panel._menus.removeMenu(this.extension.menu);
-			
-			// Causing reference error
-			//this.extension.actor.destory(); 
+			GLib.source_remove(this.extension.event);
 		}
 	}
 }
 
+
+// Extension
 function RadeonExtension() {
 	this._init();
 }
-
 RadeonExtension.prototype = {
-	//__proto__: PopupMenu.PopupBaseMenuItem.prototype,
 	__proto__: PanelMenu.Button.prototype,
 
 	_init: function() {
-		//PopupMenu.PopupBaseMenuItem.prototype._init.call(this, 0.0);
-		PanelMenu.Button.prototype._init.call(this, 0.0);
-
-		//this.extensionMeta = extensionMeta;
-		
+		PanelMenu.Button.prototype._init.call(this, 0.0);		
 		this.panelContainer = new St.BoxLayout();
 		this.actor.add_actor(this.panelContainer);
-
 		this.panelLabel = new St.Label({style_class: "radeon-label", text: _("GPU:")});
 		this.currentActivity = null;
-
-		//this.panelContainer.add(this.icon);
 		this.panelContainer.add(this.panelLabel);
-
-		this.event = GLib.timeout_add_seconds(0, 5, Lang.bind(this, this.setTemp));
-		this.setTemp();
-		
+		this.event = GLib.timeout_add_seconds(0, 5, Lang.bind(this, this.updateTempLabel));
+		this.updateTempLabel();
 		this._setPopupUI();
 	},
 
-	setTemp: function() {
+	_setPopupUI: function() {
+		let fanLabel = new PopupMenu.PopupBaseMenuItem();
+		fanLabel.addActor(new St.Label({text: _("Fan speed")}));
+
+		this._fanSlider = new FanSlider(this.getFanSpeed());
+		this._fanSlider.connect('value-changed', Lang.bind(this, this.onSliderValueChanged)); // label update
+		this._fanSlider.connect("drag-end", Lang.bind(this, this.onSliderDragEnd)); // run command
+		
+		this.menu.addMenuItem(fanLabel);
+		this.menu.addMenuItem(this._fanSlider);
+	},
+	
+	updateTempLabel: function() {
 		let output = GLib.spawn_command_line_sync("aticonfig --odgt");
 		let temp = /Temperature - ([0-9C. ]*)/.exec(output)
 		if (temp[1]) {
 			this.panelLabel.set_text("GPU: " + temp[1]);
 		} else {
-			global.log(temp);
+			Main._logDebug(temp);
 		}
 		return true;
 	},
-	
-	_setPopupUI: function() {
-		// fan label
-		let fanLabel = new PopupMenu.PopupBaseMenuItem();
-		let fanIcon = new St.Icon({
-			icon_name: 'system-run',
-			icon_type: St.IconType.SYMBOLIC,
-			style_class: 'system-status-icon'
-		});
-		fanLabel.addActor(fanIcon);
-		fanLabel.addActor(new St.Label({text: _("Fan speed")}));
-		
-		// fan slider
-		this._volume = 0.2;
-		this._fanSlider = new PopupMenu.PopupSliderMenuItem(this._volume);
-		//fanSlider.setActive(true);
-		//fanSlider._activitable = true;
-		this._fanSlider.connect('value-changed', Lang.bind(this, this._onSliderValueChanged)); // label update
-		this._fanSlider.connect("drag-end", Lang.bind(this, function(){
-			this.emit("drag-end", this._value);
-		})); // run command
-		
-		this.menu.addMenuItem(fanLabel);
-		this.menu.addMenuItem(this._fanSlider);
+
+	onSliderValueChanged: function (slider, value) {
+		this._fanSlider._label.set_text(Math.round(value*100).toString()+"%");
 	},
-
-	_onSliderValueChanged: function(slider, value) {
-		this._volume = value;
+	
+	onSliderDragEnd: function (slider) {
+		let val = Math.round(slider.value*100);
+		this.setFanSpeed(val);
+	},
+	
+	setFanSpeed: function(value) {
+		let cmd = "aticonfig --pplib-cmd \"set fanspeed 0 " + value + "\"";
+		let output = GLib.spawn_command_line_sync(cmd);
+		Main._logDebug(output);
+	},
+	
+	getFanSpeed: function() {
+		let cmd = "aticonfig --pplib-cmd \"get fanspeed 0\"";
+		let output = GLib.spawn_command_line_sync(cmd);
+		let speed = /Fan Speed: ([0-9]*)/.exec(output);
+		if (speed[1]) {
+			return speed[1];
+		} else {
+			Main._logDebug(speed+" output: "+output);
+			return 55;
+		}
 	}
-
 }
 
-function FanSlider() {
-	this._init();
+// Fan speed slider
+function FanSlider(val) {
+	this._init(val);
 }
-
 FanSlider.prototype = {
 	__proto__: PopupMenu.PopupSliderMenuItem.prototype,
 
-	_init: function() {
-		PopupMenu.PopupSliderMenuItem.prototype._init.call(this, 0.2);
-
-		//this._activitable = true;
-
+	_init: function(val) {
+		PopupMenu.PopupSliderMenuItem.prototype._init.call(this, val/100);
 		this.removeActor(this._slider);
 		this._holder = new St.BoxLayout();
-
-        /*this._icon = new St.Icon({
-            icon_type: St.IconType.SYMBOLIC,
-            icon_size: 12,
-            icon_name: icon,
-            style_class: 'system-status-icon'});
-        this._holder.add_actor(this._icon);*/
-
-        this._label = new St.Label({text: "Label", style_class: "label-class"});
+        this._label = new St.Label({text: val.toString()+"%", style_class: "label-class", reactive: true});
         this._holder.add_actor(this._label);
         this.addActor(this._slider, { span: 1, expand: false, align: St.Align.START });
         this.addActor(this._holder);
