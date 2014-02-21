f38f9b1d426d96531897b66f28d6d6b21de32460
Kamil Marszałkowski <k.marszalkowski@gmail.com>
2014-02-21 21:52:25 +0100

    Works with Gnome 3.8

--- extension.js
+++ extension.js
@@ -20,14 +20,15 @@ function ExtensionController() {
 
 		enable: function() {
 			this.extension = new RadeonExtension();
-			Main.panel._rightBox.insert_child_at_index(this.extension.actor, 0);
-			Main.panel._menus.addMenu(this.extension.menu);
+			Main.panel.addToStatusArea("radeonControl", this.extension, 0, "right");
+			Main.panel.menuManager.addMenu(this.extension.menu);
 		},
 
 		disable: function() {
-			Main.panel._rightBox.remove_actor(this.extension.actor);
-			Main.panel._menus.removeMenu(this.extension.menu);
+			Main.panel.menuManager.removeMenu(this.extension.menu);
 			GLib.source_remove(this.extension.event);
+			this.extension.actor.destroy();
+			this.extension = null;
 		}
 	}
 }
